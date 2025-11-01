0921dec277b1c7dac0d4bb081d4aecb9818b2bff
jhin1m <ducanhfake@gmail.com>
2025-11-01 13:17:32 +0700

    chore: remove test file

--- test-hook.ts
+++
@@ -1,8 +0,0 @@
-// Test file for pre-commit hook
-const test = { name: "test", value: 123, enabled: true };
-
-function badlyFormatted(a: string, b: number) {
-  return a + b;
-}
-
-export { test, badlyFormatted };
